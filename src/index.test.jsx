1eca3a037069e6404df64ebabad3eb24e306c3b2
tantaka <tomokazu.tantaka@gmail.com>
2023-12-11 15:51:35 +0900

    update

--- src/index.test.jsx
+++ src/index.test.jsx
@@ -1,4 +1,4 @@
-import { expect, test } from '@testing-library/react'
+// import { expect, test } from '@testing-library/react'
 // import App from './App'
 
 // test('renders learn react link', () => {
