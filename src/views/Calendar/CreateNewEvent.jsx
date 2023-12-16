e15c9674b574cfe0dc7addcd45417de0e70000b3
tantaka <tomokazu.tantaka@gmail.com>
2023-12-17 08:52:32 +0900

    update

--- src/views/Calendar/CreateNewEvent.jsx
+++ src/views/Calendar/CreateNewEvent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Button, Col, Form, InputGroup, Modal, Row } from 'react-bootstrap'
 import DateRangePicker from 'react-bootstrap-daterangepicker'
 
